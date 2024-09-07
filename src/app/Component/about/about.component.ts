895e2007b5990d75d0165eddd4af0224d0a335fa
shoyabkhan <shoyabkhan0508@gmail.com>
2024-09-07 10:17:01 -0700

    directive !

--- src/app/Component/about/about.component.ts
+++ src/app/Component/about/about.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+users = [
+  {
+    name: "shoyab",
+    salary: 10000,
+    status: true
+  },
+  {
+    name: "Arman",
+    salary: 1200,
+    status: false 
+  }
+]
 }
--- src/app/Component/contact/contact.component.ts
+++ src/app/Component/contact/contact.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+input: any= 'hard coded ';
+users = [
+  {name: "shoya",
+    isSingle: true,
+    salary : 18000
+  },
+  {name: "soham",
+    isSingle: false,
+    salary : 9000
+  }
+]
 
-}
+  // onChange(e:Event){
+  //   const val = (e.target as HTMLInputElement).value
+  //    this.input = val;
+    
+  }
